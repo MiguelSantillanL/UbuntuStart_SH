2be3c475713ce893396e9e9919b173c586a3da89
MiguelSantillanL <87031668+MiguelSantillanL@users.noreply.github.com>
2022-03-29 04:14:05 -0600

    Update 7_ROSCamera.sh

--- 7_ROSCamera.sh
+++ 7_ROSCamera.sh
@@ -1,6 +1,6 @@
 sudo apt-get
-sudo apt-get install ros-noetic-camera
-sudo apt-get ros-noetic-libuvc-ros
+sudo apt-get install ros-noetic-libuvc-camera
+sudo apt-get install ros-noetic-libuvc-ros 
 sudo apt-get install v4l-utils
 
 sudo apt-get update
