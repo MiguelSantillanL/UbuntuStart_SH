7a4de9d3d270866ea2e54c5016cd166b1881399b
MiguelSantillanL <87031668+MiguelSantillanL@users.noreply.github.com>
2022-03-29 00:17:05 -0600

    Update 8_workspace.sh

--- 8_workspace.sh
+++ 8_workspace.sh
@@ -5,7 +5,15 @@ source /opt/ros/noetic/setup.bash
 mkdir -p ~/laviria/catkin_ws/src
 cd ~/laviria/catkin_ws/
 catkin build
-#trabajo con AR_TRAC_ALVAR
+##        AR_TRAC_ALVAR
+cd
+git clone https://github.com/ros-perception/ar_track_alvar/tree/noetic-devel
+cd ar_track_alvar/
+
+cd
+cd ~/laviria/catkin_ws/
+catkin build
+###       trabajo con AR_TRAC_ALVAR
 cd
 git clone https://github.com/MiguelSantillanL/ROS_Cam_ARTag.git
 cd ROS_Cam_ARTag/catkin_ws/
