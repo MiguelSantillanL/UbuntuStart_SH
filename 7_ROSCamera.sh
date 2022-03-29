ba2be3461706f9f3648912d46da4e75ce99f6927
MiguelSantillanL <87031668+MiguelSantillanL@users.noreply.github.com>
2022-03-29 00:09:56 -0600

    Update 7_ROSCamera.sh

--- 7_ROSCamera.sh
+++ 7_ROSCamera.sh
@@ -1,1 +1,12 @@
 sudo apt-get
+sudo apt-get install ros-noetic-camera
+sudo apt-get ros-noetic-libuvc-ros
+sudo apt-get install v4l-utils
+
+sudo apt-get update
+sudo apt-get install ros-noetic-pcl-conversions
+sudo apt-get install ros-noetic-pcl-ros
+git clone https://github.com/ros-perception/ar_track_alvar.git -b noetic-devel
+#copy on src of the catkin_ws´s Workspace
+Catkin build
+Source devel/setup.sh
