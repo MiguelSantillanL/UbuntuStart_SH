4bdc3f48cd0969f13e9f8d71b2972cf51d3f4568
MiguelSantillanL <87031668+MiguelSantillanL@users.noreply.github.com>
2022-03-29 00:23:56 -0600

    Update extras_ROS.sh

--- extras_ROS.sh
+++ extras_ROS.sh
@@ -3,3 +3,6 @@ sudo apt-get install ros-noetic-ros-tutorials
 sudo apt-get install ros-noetic-rqt
 sudo apt-get install ros-noetic-rqt-common-plugins
 sudo apt-get install ros-noetic-rqt ros-noetic-rqt-common-plugins ros-noetic-turtlesim
+
+echo "#export ROS_HOSTNAME=DESKTOP-RI8016I" >> ~/.bashrc
+echo "#export ROS_MASTER_URI=http://miguel-Latitude:11311" >> ~/.bashrc
